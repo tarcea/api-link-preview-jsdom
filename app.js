45b32c6bf0da0d38e2206038d0b5c5e5f0ae2297
tarcea <tarcea@gmail.com>
2022-02-17 21:04:45 +0100

    openapi.yml added

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const { scraper } = require('./middlewares');
+const express = require("express");
+const cors = require("cors");
+const { scraper } = require("./middlewares");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.send('https://url-preview-gt.herokuapp.com/api?url=http://www.YOUR_URL')
+app.get("/", (req, res) => {
+  res.send("https://url-preview-gt.herokuapp.com/api?url=http://www.YOUR_URL");
 });
 
-app.get('/api', scraper, (req, res) => {
+app.get("/api", scraper, (req, res) => {
   const { title, description, url, image, icons } = res.locals.result;
   res.json({ title, description, url, image, icons });
-})
+});
 
 module.exports = app;
