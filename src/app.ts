8c22133e0cd6520856d4cb53c97f9c12f9fc07ea
tarcea <tarcea@gmail.com>
2022-07-05 15:35:13 +0200

    heroku

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
-import express, { Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { scraper } from './middlewares';
 
-const app = express();
+const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(express.static('public'));
 
+const documentation = 'https://url-preview-gt.herokuapp.com/docs.html';
+const howToUse =
+  'https://url-preview-gt.herokuapp.com/api?url=http://www.YOUR_URL';
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('https://url-preview-gt.herokuapp.com/api?url=http://www.YOUR_URL');
+  res.json({ documentation, howToUse });
 });
 
 app.get('/api', scraper, (req, res) => {
